d61c167ad6fb9aac2b0bd0eb2481f7c891ca2e76
sahilkumardev <sahilkumardev>
2024-09-27 17:02:37 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-screen max-h-screen overflow-x-hidden">
       <div className="bg-gray-900 max-w-screen-2xl flex justify-center items-center">
-        <div className="grid  grid-cols-1 sm:grid-cols-2 py-10  px-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 py-10 px-8 sm:px-16">
           <ContentBox />
           <SplineCoffee />
         </div>
--- src/components/Spline-coffee.jsx
+++ src/components/Spline-coffee.jsx
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline";
 
 const SplineCoffee = () => {
   return (
-    <div className="h-[600px] sm:h-screen"> 
+    <div className="h-[600px] sm:h-screen w-full"> 
       <Spline scene="https://prod.spline.design/JOhu71bOY89Jkhsw/scene.splinecode" />
     </div>
   );
